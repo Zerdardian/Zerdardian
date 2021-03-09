8127bb8e53a70a8b266188b3cee63c0c3946ddb5
Zerdardian <kjeldgaming562@gmail.com>
2021-03-09 17:50:41 +0100

    Design changes
    - Started on the design, made some Made the header.
    My first own header tho.
    - Menu button working, It shows and its gone after.
    ---
    Next goal, the main site. Finishing the index! And going from index.html to index.php soon. So I can do some fun things! Thats it for today

---
+++ assets/js/nav.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("menu").style.width = "400px";
+    document.getElementById("menu").style.right = "0";
+  }
+  
+  function closeNav() {
+    document.getElementById("menu").style.width = "0";
+  }
